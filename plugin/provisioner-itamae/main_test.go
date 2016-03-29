62fd322a5609612017beaa168059c50bea064e0c
Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>
2016-03-29 13:00:34 +0900

    Add test coverage for the main package.
    Signed-off-by: Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>

---
+++ plugin/provisioner-itamae/main_test.go
@@ -0,0 +1,46 @@
+/*
+ * main_test.go
+ *
+ * Copyright 2016 Krzysztof Wilczynski
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package main
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/kwilczynski/packer-provisioner-itamae/provisioner/itamae"
+)
+
+func TestMain(t *testing.T) {
+	p := itamae.Provisioner{}
+
+	func(v interface{}) {
+		if _, ok := v.(itamae.Provisioner); !ok {
+			t.Fatalf("not a itamae.Provisioner type: %s", reflect.TypeOf(v).String())
+		}
+	}(p)
+
+	defer func() {
+		r := recover()
+		if r == nil {
+			t.Errorf("should panic")
+			return
+		}
+	}()
+
+	main()
+}
